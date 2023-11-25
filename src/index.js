7dc972f1ffaf6beab569d069e0a53e8849ba470b
Dev Jain <devj5509@gmail.com>
2023-11-25 15:36:36 +0530

    custom api error handling
    Signed-off-by: Dev Jain <devj5509@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("this error arise from main index file",err);
+    })
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err);
+})
 
 
 
